5114413612a7c93230b0ab35b3b0f7a98d255a32
Marat Gumerov <gumerovm74@gmail.com>
2020-06-23 23:27:18 +0300

    Классы и наследование

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
-// const crateLink = ({path, name}) => '<a target="_blank" href="'+path+'">'+name+'</a>'
+// RootElement <= Box <= instances
 
-// const ul = document.querySelector('ul')
+class RootElement {
+  constructor(tagName = 'div') {
+    this.$el = document.createElement(tagName)
+    this.$el.style.marginBottom = '20px'
+  }
 
-// const google = '<li>' + crateLink({path:'https://google.com', name: 'Google'}) + '</li>'
-// const yandex = '<li>' + crateLink({path:'https://ya.ru', name: 'yandex'}) + '</li>'
+  hide() {
+    this.$el.style.opacity = '0'
+  }
 
+  show() {
+    this.$el.style.opacity = '1'
+  }
 
-// ul.insertAdjacentHTML('afterbegin', google)
-// ul.insertAdjacentHTML('afterbegin', yandex)
+  append() {
+    document.querySelector('.wrapper').insertAdjacentElement('beforeend', this.$el)
+  }
+}
 
-const crateLink = ({path, name}) => `<a target="_blank" href="${path}">${name}</a>`
-const ul = document.querySelector('ul')
+class Box extends RootElement {
+  constructor(color, size = 150, tagName) {
+    super(tagName)
+    this.color = color
+    this.size = size
+  }
 
-const google = `<li> ${crateLink({path:'https://google.com', name: 'Google'})}</li>`
-const yandex = `<li> ${crateLink({path:'https://ya.ru', name: 'yandex'})}</li>`
+  create() {
+    this.$el.style.background = this.color
+    this.$el.style.width = this.$el.style.height = `${this.size}px`
+    this.append()
+    return this
+  }
+}
 
+class Circle extends RootElement {
+  constructor(color) {
+    super()
+    this.color = color
+  }
 
+  create() {
+    this.$el.style.borderRadius = '50%'
+    this.$el.style.width = this.$el.style.height = `120px`
+    this.$el.style.background = this.color
+    this.append()
+    return this
+  }
+}
 
-ul.insertAdjacentHTML('afterbegin', google)
-ul.insertAdjacentHTML('afterbegin', yandex)
+const redBox = new Box('red', 100, 'div').create()
+const blueBox = new Box('blue').create()
+const circle = new Circle('green').create()
+
+circle.$el.addEventListener('mouseenter', () => {
+  circle.hide()
+})
+
+circle.$el.addEventListener('mouseleave', () => {
+  circle.show()
+})
